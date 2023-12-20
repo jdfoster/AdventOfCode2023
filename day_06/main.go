e7f003f049355719824477243f809914690be0da
Joshua D Foster <joshua.david.foster@gmail.com>
2023-12-20 11:36:35 +0000

    feat: day 6 part b

--- day_06/main.go
+++ day_06/main.go
@@ -5,11 +5,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var fn = "./day_06/input.txt"
 
-func parse(s string) ([]int, []int) {
+func parseA(s string) ([]int, []int) {
 	r := make([][]int, 2)
 	ss := strings.Split(s, "\n")
 
@@ -28,19 +30,53 @@ func parse(s string) ([]int, []int) {
 	return r[0], r[1]
 }
 
-func solve(t, d int) []int {
-	rr := make([]int, 0, t)
+func parseB(s string) (int, int) {
+	r := make([]int, 2)
+	ss := strings.Split(s, "\n")
 
-	for i := 0; i < t; i++ {
-		dt := t - i
-		x := dt * i
+	for i := 0; i < 2; i++ {
+		b := []byte(ss[i])
+		bb := make([]rune, 0, utf8.RuneCount(b))
+		for _, c := range ss[i] {
+			if unicode.IsDigit(c) {
+				bb = append(bb, c)
+			}
+		}
 
-		if x >= d {
-			rr = append(rr, i)
+		var err error
+		r[i], err = strconv.Atoi(string(bb))
+		if err != nil {
+			panic(err)
 		}
 	}
 
-	return rr
+	return r[0], r[1]
+}
+
+func solve(t, d, i int) bool {
+	dt := t - i
+	x := dt * i
+	return x > d
+}
+
+func find(t, d int) (int, int) {
+	var a, b int
+
+	for i := d / t; i < t; i++ {
+		if solve(t, d, i) {
+			a = i
+			break
+		}
+	}
+
+	for j := t; j > 0; j-- {
+		if solve(t, d, j) {
+			b = j
+			break
+		}
+	}
+
+	return a, b
 }
 
 func main() {
@@ -49,13 +85,20 @@ func main() {
 		panic(err)
 	}
 
-	ia, ib := parse(string(bb))
+	iat, iad := parseA(string(bb))
 
 	a := 1
 
-	for i := 0; i < len(ia); i++ {
-		a *= len(solve(ia[i], ib[i]))
+	for i := 0; i < len(iat); i++ {
+		x, y := find(iat[i], iad[i])
+		a *= (y - x) + 1
 	}
 
 	fmt.Println("part one value: ", a)
+
+	ibt, ibd := parseB(string(bb))
+	bx, by := find(ibt, ibd)
+	b := (by - bx) + 1
+
+	fmt.Println("part two value: ", b)
 }
