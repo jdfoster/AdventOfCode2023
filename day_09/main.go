1597f6a866ccd26831988d36d68d1c1d5e329596
Joshua D Foster <joshua.david.foster@gmail.com>
2023-12-22 10:03:42 +0000

    feat: day 9 part a

---
+++ day_09/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var fn = "./day_09/input.txt"
+
+func parse(s string) []int {
+	vv := strings.Split(s, " ")
+	rr := make([]int, len(vv))
+
+	for i, v := range vv {
+		var err error
+		rr[i], err = strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return rr
+}
+
+func diff(rr []int) []int {
+	vv := make([]int, len(rr)-1)
+
+	for i := 1; i < len(rr); i++ {
+		vv[i-1] = rr[i] - rr[i-1]
+	}
+
+	return vv
+}
+
+func predict(rr []int) int {
+	l := len(rr)
+	p := rr[l-1]
+
+	ri := rr
+	for i := 1; i < l; i++ {
+		ri = diff(ri)
+		p += ri[len(ri)-1]
+	}
+
+	return p
+}
+
+func main() {
+	f, err := os.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	var a int
+
+	for s.Scan() {
+		rr := parse(s.Text())
+		a += predict(rr)
+	}
+
+	fmt.Println("part one value: ", a)
+}
