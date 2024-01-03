2ecd2a870b3ce69a7620cffc2a7c4756e7eb4fe7
Joshua D Foster <joshua.david.foster@gmail.com>
2024-01-03 13:50:49 +0000

    refactor: day 19 part a

--- day_19/main.go
+++ day_19/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"slices"
 	"strconv"
 	"strings"
 	"unicode/utf8"
@@ -15,28 +14,11 @@ var (
 	reInstruct = regexp.MustCompile(`([a-z]{2,3})\{(([a-z][><][0-9]*:[a-zA-Z]*,?)+)([a-zA-Z]*)\}`)
 )
 
-type outcome uint8
-
-const (
-	REJECT outcome = iota
-	ACCEPT
-)
-
-func runeByIndex(s string, i int) rune {
-	for j, r := range s {
-		if j == i {
-			return r
-		}
-	}
-
-	panic(fmt.Sprintf("failed to find rune with index %d in string %q", i, s))
-}
-
 func stringByIndex(s string, i, j int) string {
 	var b strings.Builder
 
 	for k, r := range s {
-		if k > j {
+		if k >= j {
 			break
 		}
 		if k >= i {
@@ -47,127 +29,158 @@ func stringByIndex(s string, i, j int) string {
 	return b.String()
 }
 
-type shape map[rune]int
-type rule func(s ruleLookup, m shape) outcome
-type ruleLookup map[string]rule
+type op uint8
 
-func makeDestination(d string) rule {
-	return func(s ruleLookup, m shape) outcome {
-		switch d {
-		case "A":
-			return ACCEPT
-		case "R":
-			return REJECT
-		}
+const (
+	LESS_THAN op = iota
+	GREATER_THAN
+)
 
-		r, ok := s[d]
+type criterion struct {
+	component string
+	operator  op
+	value     int
+	whenTrue  string
+}
+
+type instruct struct {
+	label     string
+	criteria  []*criterion
+	whenFalse string
+}
+
+func (ins instruct) findDestination(m shape) string {
+	for _, c := range ins.criteria {
+		vi, ok := m[c.component]
 		if !ok {
-			panic(fmt.Sprintf("failed to find label in state: %s", d))
+			panic(fmt.Sprintf("failed to find component %q in shape", c.component))
 		}
 
-		return r(s, m)
-	}
-}
+		var r bool
+		switch c.operator {
+		case LESS_THAN:
+			r = vi < c.value
+		case GREATER_THAN:
+			r = vi > c.value
+		}
 
-func makeRule(c, o rune, v int, d string) func(rule) rule {
-	return func(next rule) rule {
-		return func(s ruleLookup, m shape) outcome {
-			x, ok := m[c]
-			if !ok {
-				panic(fmt.Sprintf("failed to find key in element: %s", string(c)))
-			}
-
-			var p bool
-			switch o {
-			case '>':
-				p = x > v
-			case '<':
-				p = x < v
-			default:
-				panic(fmt.Sprintf("unknown operator value: %s", string(o)))
-			}
-
-			if p {
-				return makeDestination(d)(s, m)
-			}
-
-			return next(s, m)
+		if r {
+			return c.whenTrue
 		}
 	}
+
+	return ins.whenFalse
 }
 
-func makeFilter(gg []string, d string) rule {
-	rr := make([]func(rule) rule, len(gg))
+func newInstruct(s string) *instruct {
+	aa := reInstruct.FindAllStringSubmatch(s, -1)
+	rr := strings.Split(strings.TrimSuffix(aa[0][2], ","), ",")
 
-	for i, g := range gg {
-		c := runeByIndex(g, 0)
-		o := runeByIndex(g, 1)
-		j := strings.IndexRune(g, ':')
-		k := utf8.RuneCountInString(g)
-		d := stringByIndex(g, j+1, k)
-		vi := stringByIndex(g, 2, j-1)
-		v, err := strconv.Atoi(vi)
+	r := &instruct{
+		label:     aa[0][1],
+		criteria:  make([]*criterion, len(rr)),
+		whenFalse: aa[0][4],
+	}
+
+	for i, ri := range rr {
+		j := strings.IndexRune(ri, ':')
+		k := utf8.RuneCountInString(ri)
+		vi := stringByIndex(ri, 2, j)
+
+		r.criteria[i] = &criterion{
+			component: stringByIndex(ri, 0, 1),
+			whenTrue:  stringByIndex(ri, j+1, k),
+		}
+
+		switch op := stringByIndex(ri, 1, 2); op {
+		case "<":
+			r.criteria[i].operator = LESS_THAN
+		case ">":
+			r.criteria[i].operator = GREATER_THAN
+		default:
+			panic(fmt.Sprintf("unknown operator %q", op))
+		}
+
+		var err error
+		r.criteria[i].value, err = strconv.Atoi(vi)
 		if err != nil {
 			panic(err)
 		}
-
-		rr[i] = makeRule(c, o, v, d)
-	}
-
-	slices.Reverse(rr)
-
-	var r rule
-	for i, ri := range rr {
-		if i == 0 {
-			r = ri(makeDestination(d))
-			continue
-		}
-
-		r = ri(r)
 	}
 
 	return r
 }
 
-func parse(s string) (ruleLookup, []shape) {
-	part := strings.Split(s, "\n\n")
-	qq := strings.Split(part[0], "\n")
-	rr := make(ruleLookup, len(qq))
+type outcome uint8
 
-	for _, q := range qq {
-		mm := reInstruct.FindAllStringSubmatch(q, -1)
-		gg := strings.Split(strings.TrimSuffix(mm[0][2], ","), ",")
-		h := mm[0][4]
-		l := mm[0][1]
-		rr[l] = makeFilter(gg, h)
+const (
+	REJECT outcome = iota
+	ACCEPT
+)
+
+type lookup map[string]*instruct
+type shape map[string]int
+
+func (u lookup) validateShape(s shape, l string) outcome {
+	ins, ok := u[l]
+	if !ok {
+		panic(fmt.Sprintf("failed to find label %q", l))
 	}
 
+	d := ins.findDestination(s)
+
+	switch d {
+	case "A":
+		return ACCEPT
+	case "R":
+		return REJECT
+	}
+
+	return u.validateShape(s, d)
+}
+
+func newLookup(ss []string) lookup {
+	ll := make(map[string]*instruct, len(ss))
+
+	for _, s := range ss {
+		r := newInstruct(s)
+		ll[r.label] = r
+	}
+
+	return ll
+}
+
+func parse(f string) (lookup, []shape) {
+	part := strings.Split(f, "\n\n")
+	qq := strings.Split(part[0], "\n")
+	rr := newLookup(qq)
+
 	kk := strings.Split(part[1], "\n")
-	ee := make([]shape, len(kk))
+	ss := make([]shape, len(kk))
 
+	var n int
 	for i, k := range kk {
-		ee[i] = make(map[rune]int, 3)
-		hh := strings.Split(strings.TrimSuffix(strings.TrimPrefix(k, "{"), "}"), ",")
+		ss[i] = make(map[string]int, 3)
+		cc := strings.Split(strings.TrimSuffix(strings.TrimPrefix(k, "{"), "}"), ",")
 
-		for _, h := range hh {
-			if h == "" {
+		for _, c := range cc {
+			if c == "" {
+				n++
 				continue
 			}
 
-			r := runeByIndex(h, 0)
-			k := utf8.RuneCountInString(h)
-			vi := stringByIndex(h, 2, k)
-			v, err := strconv.Atoi(vi)
+			vi := strings.Split(c, "=")
+			k, v := vi[0], vi[1]
+
+			var err error
+			ss[i][k], err = strconv.Atoi(v)
 			if err != nil {
 				panic(err)
 			}
-
-			ee[i][r] = v
 		}
-
 	}
 
-	return rr, ee[:len(ee)-1]
+	return rr, ss[:len(ss)-n]
 }
 
 func main() {
@@ -181,11 +194,7 @@ func main() {
 	var a int
 
 	for _, s := range ss {
-		r, ok := rr["in"]
-		if !ok {
-			panic("missing starting rule")
-		}
-		if r(rr, s) == ACCEPT {
+		if rr.validateShape(s, "in") == ACCEPT {
 			var sum int
 			for _, v := range s {
 				sum += v
